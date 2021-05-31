0539f79b1f321cb5323c9d51e0631e8240d2b1f6
lucas mendes <lucas.mendes@external.fcagroup.com>
2021-05-31 19:49:40 -0300

    commit inicial

---
+++ src/main/java/com/example/IncidentSystem/IncidentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.IncidentSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncidentSystemApplication.class, args);
+	}
+
+}
