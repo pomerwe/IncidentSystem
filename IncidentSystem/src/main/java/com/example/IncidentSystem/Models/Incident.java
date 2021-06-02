26fc502764cf52500fee0e0eb0f30783502a4f25
lucas mendes <lucas.mendes@external.fcagroup.com>
2021-06-01 21:11:24 -0300

    commit incident

---
+++ IncidentSystem/src/main/java/com/example/IncidentSystem/Models/Incident.java
@@ -0,0 +1,14 @@
+package com.example.IncidentSystem.Models;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+
+public class Incident {
+
+    @Id
+    @Column
+    private long id;
+
+    
+
+}
