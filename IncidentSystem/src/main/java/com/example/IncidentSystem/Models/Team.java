7e5912a6cab8445bd398e10015351e06a323fa0d
Estevão Alves <pomerwe@gmail.com>
2021-06-01 21:10:38 -0300

    Classes Employee e Team

--- IncidentSystem/src/main/java/com/example/IncidentSystem/Models/Team.java
+++ IncidentSystem/src/main/java/com/example/IncidentSystem/Models/Team.java
@@ -1,5 +1,34 @@
 package com.example.IncidentSystem.Models;
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+@Entity
+@Table(name = "Team")
 public class Team {
 
+	@Id
+	@Column
+	private int id;
+	
+	@Column
+	private String name;
+	
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "team")
+	private Set<Service> services;
+	
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "team")
+	@JsonIgnore
+	private Set<Employee> employees;
 }
