ee346b6cfd1ceb1ebdc99e41741a5bd31e6bc456
lucas mendes <lucas.mendes@external.fcagroup.com>
2021-06-01 21:49:55 -0300

    commit 'implements Serializable {'

--- IncidentSystem/src/main/java/com/example/IncidentSystem/Models/BusinessRole.java
+++ IncidentSystem/src/main/java/com/example/IncidentSystem/Models/BusinessRole.java
@@ -4,10 +4,11 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "business_role")
-public class BusinessRole {
+public class BusinessRole implements Serializable {
 	
 	@Id
 	@Column
